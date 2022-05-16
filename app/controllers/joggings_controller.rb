306a20ac4af0ad41a5d58e493e5fbe5ffa544032
Mohamedtarekmetwally@gmail.com <51732423+MohamedTarekMetwally@users.noreply.github.com>
2022-05-16 03:34:46 +0200

    done with the roles feature and making the admin access the whole joggings and users can only access their joggings......

--- app/controllers/joggings_controller.rb
+++ app/controllers/joggings_controller.rb
@@ -1,5 +1,6 @@
 class JoggingsController < ApplicationController
   before_action :set_jogging, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except:[:index, :show]
 
   # GET /joggings or /joggings.json
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_15_202249) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_16_010646) do
   create_table "joggings", force: :cascade do |t|
     t.datetime "date", precision: nil
     t.integer "distance"
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_15_202249) do
     t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "position"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
